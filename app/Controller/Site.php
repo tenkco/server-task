0e6606791e2fc4134d2ad9ad075d5b534f62e6eb
tenkco <sytnikkv@gmail.com>
2026-04-17 16:45:31 +0700

    working with sessions and redirects

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -22,10 +22,11 @@ class Site
 
     public function signup(Request $request): string
     {
-        if ($request->method==='POST' && User::create($request->all())){
-            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        if ($request->method === 'POST' && User::create($request->all())) {
+            app()->route->redirect('/go');
         }
         return new View('site.signup');
     }
+
 
 }
