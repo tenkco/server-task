5f3fd0c2414d20a29a832750f59c116ef8726701
tenkco <sytnikkv@gmail.com>
2026-04-17 12:51:49 +0700

    registration function

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,6 +5,7 @@ namespace Controller;
 use Model\Post;
 use Src\View;
 use Src\Request;
+use Model\User;
 
 class Site
 {
@@ -14,9 +15,17 @@ class Site
         return (new View())->render('site.post', ['posts' => $posts]);
     }
 
-
     public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
     }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method==='POST' && User::create($request->all())){
+            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        }
+        return new View('site.signup');
+    }
+
 }
