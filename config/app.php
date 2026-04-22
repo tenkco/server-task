ff62cdd30525c3a070a133bac61903aef542f144
tenkco <sytnikkv@gmail.com>
2026-04-22 13:58:10 +0700

    data filtering

--- config/app.php
+++ config/app.php
@@ -12,6 +12,10 @@ return [
     'validators' => [
         'required' => \Validators\RequireValidator::class,
         'unique' => \Validators\UniqueValidator::class
-    ]
+    ],
+    'routeAppMiddleware' => [
+        'trim' => \Middlewares\TrimMiddleware::class,
+    ],
+
 
 ];
