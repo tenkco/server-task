85b871719c15a2fb61e60185236b401291e5a2d9
tenkco <sytnikkv@gmail.com>
2026-04-23 23:50:26 +0700

    integration

--- app/Middlewares/SpecialCharsMiddleware.php
+++ app/Middlewares/SpecialCharsMiddleware.php
@@ -2,15 +2,17 @@
 namespace Middlewares;
 
 use Src\Request;
+use function Collect\collection;
 
 class SpecialCharsMiddleware
 {
-    public function handle(Request $request):Request
+    public function handle(Request $request): Request
     {
-        foreach ($request->all() as $key => $value) {
-            $request->set($key, is_string($value) ? htmlspecialchars($value) : $value);
-        }
+        collection($request->all())
+            ->each(function ($value, $key, $request) {
+                $request->set($key, is_string($value) ? htmlspecialchars($value) : $value);
+            }, $request);
+
         return $request;
     }
 }
-
